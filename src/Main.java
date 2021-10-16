7ebc509dea5386b7baf1f422a711c1b503097e3a
MrSemyon12 <denisenko.box19@gmail.com>
2021-10-17 00:17:41 +1000

    RandomStringChooser

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+//
+//        ClimbingClub mountainHike = new ClimbingClub();
+//        mountainHike.addClimb("Kilimanjaro", 120);
+//        mountainHike.addClimb("Everest", 145);
+//        mountainHike.addClimb("Pikes Peak", 80);
 
-        ClimbingClub mountainHike = new ClimbingClub();
-        mountainHike.addClimb("Kilimanjaro", 120);
-        mountainHike.addClimb("Everest", 145);
-        mountainHike.addClimb("Pikes Peak", 80);
-
-
+//        String[] arrayList = {"wheels", "on", "the", "bus"};
+//        RandomStringChooser stringChooser = new RandomStringChooser(arrayList);
+//        for (int i = 0; i < 6; i++)
+//        {
+//            System.out.println(stringChooser.getNext());
+//        }
 
     }
 }
---
+++ src/RandomStringChooser.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class RandomStringChooser {
+
+    private ArrayList<String> wordArray;
+
+    public RandomStringChooser(String[] wordArray){
+        this.wordArray = new ArrayList<>();
+        for (String current: wordArray) {
+            this.wordArray.add(current);
+        }
+    }
+
+    public String getNext(){
+        if (this.wordArray.size() == 0)
+        {
+            return null;
+        }
+        else
+        {
+            Random random = new Random();
+            int delIndex = random.nextInt(wordArray.size());
+            String delString = this.wordArray.get(delIndex);
+            this.wordArray.remove(delIndex);
+            return delString;
+        }
+    }
+
+}
