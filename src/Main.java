43ea302717831815fe3297db6b3905e9d820df53
MrSemyon12 <denisenko.box19@gmail.com>
2021-11-01 14:42:43 +1000

    regular

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import Task11or12.Bowl;
-import Task11or12.MyThread;
-import java.util.ArrayList;
-
 public class Main {
-
     public static void main(String[] args) {
 
     }
--- src/Task8/DataStorage.java
+++ src/Task8/DataStorage.java
@@ -1,4 +1,4 @@
-package Task7;
+package Task8;
 
 public class DataStorage {
 
